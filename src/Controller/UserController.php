efbf5b6ecca259ad190f029d5863fbf9356a4260
santamaria <pauliix.santamaria@gmail.com>
2020-07-03 16:12:55 +0200

    change url for register upload

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -26,37 +26,45 @@ class UserController extends AbstractController
     }
 
     /**
-     * @Route("/register", name="register", methods={"POST"})
+     * @Route("api/register", name="register", methods={"POST"})
      */
     public function register(UserPasswordEncoderInterface $encoder, Request $request, SerializerInterface $serializer)
     {
-        $data = $request->getcontent();
+        $securityContext = $this->container->get('security.authorization_checker');
+        if($securityContext->isGranted('IS_AUTHENTICATED_FULLY')){
+            $data = $request->getcontent();
 
-        $userData = $serializer->deserialize($data, User::class, 'json' );
+            $userData = $serializer->deserialize($data, User::class, 'json' );
 
-        dump($data);
+            $em = $this->getDoctrine()->getManager();
+            $truc=$this->userRepository->findOneBy([
+                "username" => $userData->getUsername(),
+            ]);
+            
+            if($truc){
+                return new JsonResponse('userName already exist',409, [], true );
+            };
 
-        $em = $this->getDoctrine()->getManager();
-        $truc=$this->userRepository->findOneBy([
-            "username" => $userData->getUsername(),
-        ]);
+
+            $user = new User;
+
+            $user->setUsername($userData->getUsername());
+            $user->setPassword($encoder->encodePassword($user, $userData->getPassword()));
+
         
-        if($truc){
-            return new JsonResponse('userName already exist',409, [], true );
-        };
-
-
-        $user = new User;
-
-        $user->setUsername($userData->getUsername());
-        $user->setPassword($encoder->encodePassword($user, $userData->getPassword()));
-
-       
-        $em->persist($user);
-        $em->flush();
-    
-        return new JsonResponse('ok', 201, [
-             "tqt"
-        ], true);
+            $em->persist($user);
+            $em->flush();
+        
+            return new JsonResponse('log in success', 201, [
+                
+            ], true);
+        }
+        else{
+            return new JsonResponse('please log in', 503, [
+                
+           ], true);
+        }
+        
+        
     }
 }
